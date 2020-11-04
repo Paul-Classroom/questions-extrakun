1991c773989ef58ea904f64813b9a8e3496973da
Kun <extrakun@yahoo.com.sg>
2020-11-04 07:36:06 +0000

    Added new stuff

--- q1/q1.js
+++ q1/q1.js
@@ -2,7 +2,7 @@ const {prompt} = require('../common.js');
 
 async function main() {
     // write your code here
-    let m = parseInt(await prompt());
+    let m = parseInt(await prompt("Enter a number"));
     let n = parseInt(await prompt());
     console.log(n + m);
 }
